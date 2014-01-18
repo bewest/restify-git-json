d13d9b9555bdddc9c2f7fa48a81e1c7bdaa85310
Ben West <bewest@gmail.com>
2014-01-17 18:01:35 -0800

    Example server, customizing users

---
+++ examples/custom-users.js
@@ -0,0 +1,37 @@
+
+var gitServer = require('../server');
+
+function customUser (profile, next) {
+  // customize description
+  // actually you can replace the entire thing, supports async fetching.
+  profile.description = "Hello world!";
+  next(null, profile);
+}
+
+function configure (hook, next) {
+  hook.map(customUser);
+  next( );
+}
+
+function createServer( ) {
+  var env = require('../env');
+
+  var server = gitServer(env);
+
+  server.events.on('profile', configure);
+  return server;
+}
+module.exports = createServer;
+
+if (!module.parent) {
+  console.log('main');
+  var server = createServer( );
+
+  var env = require('../env');
+  var port = env.PORT || 6886;
+  server.listen(port, function( ) {
+    console.log('listening on', server.name, server.url);
+  });
+
+}
+
