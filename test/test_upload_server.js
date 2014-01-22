fe05cfa2d2c8529d2103985e37456af11c955952
Ben West <bewest@gmail.com>
2014-01-22 15:48:49 -0800

    WIP tests: stuck on upload testing

--- test/test_upload_server.js
+++ test/test_upload_server.js
@@ -1,6 +1,29 @@
 
 var restify = require('restify');
+var clients = require('restify/lib/clients');
 var es = require('event-stream');
+var es = require('fs');
+var mimeStream = require('mime-multipart-stream');
+function testContent (T) {
+  var fileOne = es.readArray(["dummy content hello world text"]);
+  var fileTwo = fs.createReadStream('../README.markdown');
+  var pre = "====";
+  var boundary =  pre + pre + pre + "TIDEPOOL" + "TESTBOUNDARY" + "EOF";
+  var content = {fileOne: fileOne, fileTwo: fileTwo};
+  var stream = mimeStream({boundary: boundary, type: 'form-data'});
+  function make (name, part) {
+    var o = {
+      disposition: 'form-data; name="' + name + '"; filename="'+ name + '"'
+    , type: 'text/plain; charset=UTF-8'
+    , transferEncoding: 'quoted-printable'
+    , body: part
+    };
+    return o;
+  }
+  stream.add(make('testfileOne', content.fileOne));
+  stream.add(make('testfileTwo', content.fileTwo));
+  return stream;
+}
 function createClient (opts) {
   var client = restify.createJsonClient(opts);
   function help (fn) {
@@ -21,8 +44,37 @@ function createClient (opts) {
     return client.post(url, update, fn);
   }
   function uploadContent (profile, content, fn) {
-    var url = '/repos/' + profile.handle + '/test/upload';
-    return client.post(url, content, fn);
+    var name = profile.handle;
+    var url = '/repos/' + name + '/test/upload';
+    var config = {
+      'content-type': 'multipart/form-data'
+    };
+    // options.
+    var mimes = testContent(content);
+    var str = restify.createHttpClient(opts);
+    // function write (config, cb) { client.request(options, begin); }
+    var req = str.post(config, begin);
+    // req.on('error', error);
+    // es.pipeline(mimes, req);
+    // content.pipe(req);
+    // return str.post.apply(client, post(url, content, start);
+    function begin (err, req) {
+      console.log("UPLOAD REQUEST", 'error', err, "RES", req);
+      es.pipeline(mimes, req);
+      req.on('result', done);
+      req.on('error', error);
+    }
+    // var ERROR;
+    function error (err, res) {
+      // ERROR = err;
+      console.log("ERROR ERROR", err);
+      fn(err, null);
+    }
+    function done (err, res) {
+      console.log("UPLOAD RESPONSE", 'error', err, "RES", res);
+      es.pipeline(res, es.writeArray(fn));
+      // fn(null, result);
+    }
   }
   function download (url) {
 
@@ -34,6 +86,7 @@ function createClient (opts) {
   client.userInfo = userInfo;
   client.createUser = createUser;
   client.updateUser = updateUser;
+  client.uploadContent = uploadContent;
   return client;
 }
 module.exports.before = function ( ) {
@@ -82,22 +135,35 @@ describe("restify-git-json server", function ( ) {
       testFetchUser(client, foobarUser.handle);
     }
     function testFetchUser (client, name) {
-      it('should fetch a created user', function (done) {
-        client.userInfo( function (err, req, res, body) {
-          console.log("userInfo", body);
-          body.should.be.ok;
-          body.user.name.should.be.ok;
-          profile = body;
-          done( );
-          testUpdateUser( );
+      it('should sync creation', function ( ) {
+        it('should fetch a created user', function (done) {
+          client.userInfo( function (err, req, res, body) {
+            console.log("userInfo", body);
+            body.should.be.ok;
+            body.user.name.should.be.ok;
+            profile = body;
+            done( );
+            // testUploadContent(pro);
+          });
         });
-      });
+        it('should then update', function (done) {
+          console.log('should then update');
+          testUpdateUser(done);
+        });
+        it('should then upload', function (done) {
+          testUploadContent(profile, done);
+          // done( );
+        });
+      })
     }
-    function testUpdateUser ( ) {
-      it('should update user', function (done) {
+
+    function testUpdateUser (finish) {
+      console.log("XXXXX");
+      // it('should update user', function (done) {
         console.log("PROFILE FOR UPDATE", profile);
         var update = JSON.parse(JSON.stringify(profile));
         update.user.name = "Bar Update";
+        update.user.email = "update@tidepool.io";
         update.user.data = { custom: 'property' };
         update.user.user = JSON.parse(JSON.stringify(update.user))
         name = update.name;
@@ -107,10 +173,24 @@ describe("restify-git-json server", function ( ) {
           result.user.name.should.equal(update.user.name)
           result.user.data.should.be.ok;
           result.user.data.custom.should.equal(update.user.data.custom);
-          done( );
+          if (finish) { finish(result); }
+          // done( );
         });
-      });
+      // });
     }
+
+    function testUploadContent (profile, fn) {
+    // describe("uploaded content", function ( ) {
+        console.log("YYYY");
+        it('user should be able to upload content into git', function ( ) {
+          client.uploadContent(profile, 'test', function (err, results) {
+            console.log("UPLOADED", results);
+            fn(err, results);
+          });
+        });
+    // });
+    }
+
     it('should 404 non-existent users', function (done) {
       client.userInfo('xx-no-exist', function (err, req, res, body) {
         // console.log("404 ERR", 'error', err, 'body', body);
@@ -143,7 +223,7 @@ function testCreateUser (client, update, fn) {
       fn(R.err, R.result);
     }
   }
-  describe("user creation REST api", function ( ) {
+  // describe("user creation REST api", function ( ) {
     it('should create a user', function (done) {
       var name = update.handle;
       client.createUser(name, update, function (err, req, res, result) {
@@ -169,5 +249,5 @@ function testCreateUser (client, update, fn) {
         finish(err, result);
       });
     });
-  });
+  // });
 }
