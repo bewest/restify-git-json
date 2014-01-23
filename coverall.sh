35f4c3983a6a2d605d17dd59675625d8214d1b32
Ben West <bewest@gmail.com>
2014-01-22 21:18:42 -0800

    include forgotten dependency

--- coverall.sh
+++ coverall.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 if [[ -f .cover_env ]] ; then
-  env
+  # env
+  env $(cat .cover_env) ./node_modules/.bin/coveralls
   # env $(cat .cover_env) ./node_modules/.bin/coveralls
-  # env $(cat .cover_env) ./node_modules/.bin/coveralls
+  exit 0;
 fi
 ./node_modules/.bin/coveralls
 
